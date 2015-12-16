9c21c8ff61c71ffdc965b162f3cca57a79430217
prasanna.boga <prasofty@gmail.com>
2015-12-16 16:38:57 +0530

    fixed rspec models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,13 @@
 class Product < ActiveRecord::Base
 
-  monetize :price_cents
+  monetize :price_cents, allow_nil: false, numericality: {
+               greater_than_or_equal_to: 0
+           }
   mount_uploader :image, ProductImageUploader
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 100 }
+  validates :price_cents, presence: true, numericality: { greater_than: 0 }
   validates :image, presence: true
-
 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,6 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'devise'
-require '../spec/support/controller_macros'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -27,8 +26,6 @@ require '../spec/support/controller_macros'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
-  config.extend ControllerMacros, type: :controller
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
